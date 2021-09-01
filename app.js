2eaf5c4d4902137df06cb9ec8f5f3999c8c35a7d
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-01 10:00:41 +0900

    #3.7 CSS in Javascript part Two

--- app.js
+++ app.js
@@ -2,14 +2,12 @@ const h1El = document.querySelector('h1');
 
 
 function handleTitleClick () {
-  const currentColor = h1El.style.color;
-  let newColor;
-  if (currentColor === 'blue') {
-    newColor = 'tomato';
+  const clickedClass = 'clicked';
+  if (h1El.className === clickedClass) {
+    h1El.className = '';
   } else {
-    newColor = 'blue';
-  } 
-  h1El.style.color = newColor;
+    h1El.className = clickedClass;
+  }
 }
 
 h1El.addEventListener('click', handleTitleClick);

07492319c41304e97acb30e1d9d1fb5c08b0d811
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-01 09:26:14 +0900

    #3.6 More Events

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const h1El = document.querySelector('h1');
 console.dir(h1El);
 function colorChange() {
   h1El.style.color = 'blue';
-};
+}
 
 h1El.addEventListener('click', colorChange);
 h1El.addEventListener('mouseenter', function () {
@@ -12,3 +12,24 @@ h1El.addEventListener('mouseenter', function () {
 h1El.addEventListener('mouseleave', () => {
   h1El.innerHTML = 'mouse is gone~!';
 })
+
+function handleWindowResize () {
+  document.body.style.backgroundColor = 'salmon';
+}
+
+function handleWindowCopy () {
+  alert('copier!');
+}
+
+function handleWindowOffline () {
+  alert('SOS no WIFI');
+}
+
+function handleWindowOnline () {
+  alert('ALL GOOD');
+}
+
+window.addEventListener('resize', handleWindowResize);
+window.addEventListener('copy',handleWindowCopy);
+window.addEventListener('offline',handleWindowOffline);
+window.addEventListener('online',handleWindowOnline);

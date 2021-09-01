c0468c91eed10431642b28aad570b15bd286f0ec
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-01 09:46:39 +0900

    3.6 CSS in Javascript

--- app.js
+++ app.js
@@ -1,35 +1,15 @@
 const h1El = document.querySelector('h1');
 
-console.dir(h1El);
-function colorChange() {
-  h1El.style.color = 'blue';
+
+function handleTitleClick () {
+  const currentColor = h1El.style.color;
+  let newColor;
+  if (currentColor === 'blue') {
+    newColor = 'tomato';
+  } else {
+    newColor = 'blue';
+  } 
+  h1El.style.color = newColor;
 }
 
-h1El.addEventListener('click', colorChange);
-h1El.addEventListener('mouseenter', function () {
-  h1El.innerText = 'mouse is here~!';
-});
-h1El.addEventListener('mouseleave', () => {
-  h1El.innerHTML = 'mouse is gone~!';
-})
-
-function handleWindowResize () {
-  document.body.style.backgroundColor = 'salmon';
-}
-
-function handleWindowCopy () {
-  alert('copier!');
-}
-
-function handleWindowOffline () {
-  alert('SOS no WIFI');
-}
-
-function handleWindowOnline () {
-  alert('ALL GOOD');
-}
-
-window.addEventListener('resize', handleWindowResize);
-window.addEventListener('copy',handleWindowCopy);
-window.addEventListener('offline',handleWindowOffline);
-window.addEventListener('online',handleWindowOnline);
+h1El.addEventListener('click', handleTitleClick);

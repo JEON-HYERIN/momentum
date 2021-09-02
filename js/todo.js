6013a8f63f603faa7b020f5abed256ad75964215
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-02 10:44:11 +0900

    #7.3 Saving To Dos

--- js/todo.js
+++ js/todo.js
@@ -1,11 +1,18 @@
 const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
+const toDos = [];
+
+function saveToDos () {
+  localStorage.setItem('todos', JSON.stringify(toDos));
+}
 
 function deleteToDo (event) {
   const li = event.target.parentElement;
   li.remove();
 }
+
+
 
 function paintToDo (newTodo) {
   const li = document.createElement('li');
@@ -23,7 +30,9 @@ function handleToDoSubmit (event) {
   event.preventDefault();
   const newTodo = toDoInput.value;
   toDoInput.value = '';
+  toDos.push(newTodo);
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit',handleToDoSubmit);

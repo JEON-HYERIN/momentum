122e627e3f67dc742dc40ab8f7b3652300ca3a1c
JEON-HYERIN <jeonhyelin19951118@gmail.com>
2021-09-02 11:47:15 +0900

    #7.4 Loading To Dos part One

--- js/todo.js
+++ js/todo.js
@@ -1,18 +1,20 @@
 const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
+
+const TODOS_KEY = 'todos';
+
 const toDos = [];
 
 function saveToDos () {
-  localStorage.setItem('todos', JSON.stringify(toDos));
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+  
 }
 
 function deleteToDo (event) {
   const li = event.target.parentElement;
   li.remove();
 }
-
-
 
 function paintToDo (newTodo) {
   const li = document.createElement('li');
@@ -36,3 +38,12 @@ function handleToDoSubmit (event) {
 }
 
 toDoForm.addEventListener('submit',handleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    parsedToDos.forEach(item => {
+      console.log(item);
+    });
+}
